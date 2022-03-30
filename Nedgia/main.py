8f58280c72b101701eb5567256f2af55f1054739
elskater98 <elskater98@gmail.com>
2022-03-30 16:35:58 +0200

    Refactor code

--- Nedgia/main.py
+++ Nedgia/main.py
@@ -1,11 +1,10 @@
-if __name__ == '__main__':
+import pandas as pd
 
-    # Imports
-    import pandas as pd
+tz_info = 'Europe/Madrid'
 
-    tz_info = 'Europe/Madrid'
 
-    df = pd.read_excel('data/Generalitat Extracción_2018.xlsx', skiprows=2)
+def script_input():
+    df = pd.read_excel('../data/Generalitat Extracción_2018.xlsx', skiprows=2)
 
     # Transform Raw Data
 
@@ -27,13 +26,96 @@ if __name__ == '__main__':
 
     df = df[['CUPS', 'ts', 'measurementStart', 'measurementEnd', 'measurementValue', 'Tipo Lectura']]
 
+    return df
+
+
+def split_dataframe(dataframe):
+    list_of_dataframes = []
+    if True in dataframe['is_gap'].unique():
+
+        last_status = None
+        index = None
+
+        for row in dataframe.itertuples():
+
+            if last_status is None:
+                index = 0
+                list_of_dataframes.append([])
+
+            if last_status is not None and row.is_gap is True:
+                index += 1
+                list_of_dataframes.append([])
+
+            if last_status is not None and row.is_gap is False and last_status is True:
+                index += 1
+                list_of_dataframes.append([])
+
+            list_of_dataframes[index].append(row)
+            last_status = row.is_gap
+
+        for i in range(len(list_of_dataframes)):
+            new_df = pd.DataFrame(list_of_dataframes[i])
+            new_df.rename(columns={'_6': 'Tipo Lectura'}, inplace=True)
+            list_of_dataframes[i] = new_df
+
+    else:
+        list_of_dataframes.append(dataframe)
+
+    return list_of_dataframes
+
+
+def harmonize_dataframe(dataframe):
+    if 'REAL' in dataframe['Tipo Lectura'].unique():
+        # Drop n rows until REAL
+        first_real = dataframe[dataframe['Tipo Lectura'] == 'REAL'].index[0]
+
+        df_i = dataframe.loc[first_real:]
+    else:
+        return
+
+    # Filter by Tipo Lectura == REAL
+    df_i_real = df_i[df_i["Tipo Lectura"] == 'REAL'].copy()
+
+    df_i_real['cumsum'] = df_i_real['measurementValue'].cumsum()
+
+    df_i_real.set_index('measurementStart_dt', inplace=True)
+
+    interpolate_df = df_i_real[['cumsum']].resample('1D').interpolate()
+
+    interpolate_df['diff'] = interpolate_df['cumsum'].diff()
+
+    interpolate_df.at[interpolate_df.index[0], 'diff'] = interpolate_df.iloc[0]['cumsum']
+
+    interpolate_df['measurementEnd_dt'] = interpolate_df.index + pd.Timedelta(hours=23)
+
+    interpolate_df.reset_index(inplace=True)
+
+    interpolate_df.rename(columns={'measurementStart_dt': 'measurementStart', 'diff': 'measurementValue',
+                                   'measurementEnd_dt': 'measurementEnd'}, inplace=True)
+    interpolate_df['CUPS'] = cups
+
+    interpolate_df['measurementStart'] = interpolate_df['measurementStart'].astype('int') / 10 ** 9
+    interpolate_df['measurementStart'] = interpolate_df['measurementStart'].astype('int')
+
+    interpolate_df['measurementEnd'] = interpolate_df['measurementEnd'].astype('int') / 10 ** 9
+    interpolate_df['measurementEnd'] = interpolate_df['measurementEnd'].astype('int')
+
+    return interpolate_df[['CUPS', 'measurementStart', 'measurementEnd', 'measurementValue']]
+
+
+if __name__ == '__main__':
+    df = script_input()
+
     # Group by CUPS
     for cups, sub_df in df.groupby('CUPS'):
+        # Convert Timestamps to Datetime
         sub_df.sort_values(by=['measurementStart'], inplace=True)
         sub_df['measurementStart_dt'] = pd.to_datetime(sub_df['measurementStart'], unit='s').dt.tz_localize(
             'UTC').dt.tz_convert(tz_info)
+
         sub_df['measurementEnd_dt'] = pd.to_datetime(sub_df['measurementEnd'], unit='s').dt.tz_localize(
             'UTC').dt.tz_convert(tz_info)
+
         sub_df['measurementEnd_dt'] -= pd.Timedelta(hours=23)
 
         # https://towardsdatascience.com/efficiently-iterating-over-rows-in-a-pandas-dataframe-7dd5f9992c01
@@ -50,71 +132,13 @@ if __name__ == '__main__':
         sub_df.loc[sub_df["measurementStart_dt_shifted"].isnull(), "is_gap"] = False
 
         # Split Dataframes
-        list_of_dataframes = []
-        if True in sub_df['is_gap'].unique():
-
-            last_status = None
-            index = None
-
-            for row in sub_df.itertuples():
-
-                if last_status is None:
-                    index = 0
-                    list_of_dataframes.append([])
-
-                if last_status is not None and row.is_gap is True:
-                    index += 1
-                    list_of_dataframes.append([])
-
-                if last_status is not None and row.is_gap is False and last_status is True:
-                    index += 1
-                    list_of_dataframes.append([])
-
-                list_of_dataframes[index].append(row)
-                last_status = row.is_gap
-
-            for i in range(len(list_of_dataframes)):
-                new_df = pd.DataFrame(list_of_dataframes[i])
-                new_df.rename(columns={'_6': 'Tipo Lectura'}, inplace=True)
-                list_of_dataframes[i] = new_df
-
-        else:
-            list_of_dataframes.append(sub_df)
+        list_of_dataframes = split_dataframe(dataframe=sub_df)
 
         # Loop for each dataframe
         for df_i in list_of_dataframes:
-            # Calculate only if dataframe has at least one REAL value
-            if 'REAL' in df_i['Tipo Lectura'].unique():
-                # Drop n rows until tipo lectura = REAL
-                first_real = df_i[df_i['Tipo Lectura'] == 'REAL'].index[0]
+            df_res = harmonize_dataframe(df_i)
+            print(df_res)
 
-                df_i = df_i.loc[first_real:]
-            else:
-                continue
-
-            df_i_real = df_i[df_i["Tipo Lectura"] == 'REAL'].copy()
-            df_i_real['cumsum'] = df_i_real['measurementValue'].cumsum()
-
-            df_i_real.set_index('measurementStart_dt', inplace=True)
-
-            interpolate_df = df_i_real[['cumsum']].resample('1D').interpolate()
-
-            interpolate_df['diff'] = interpolate_df['cumsum'].diff()
-
-            interpolate_df.at[interpolate_df.index[0], 'diff'] = interpolate_df.iloc[0]['cumsum']
-
-            interpolate_df['measurementEnd_dt'] = interpolate_df.index + pd.Timedelta(hours=23)
-
-            interpolate_df.reset_index(inplace=True)
-
-            interpolate_df.rename(columns={'measurementStart_dt': 'measurementStart', 'diff': 'measurementValue',
-                                           'measurementEnd_dt': 'measurementEnd'}, inplace=True)
-            interpolate_df['CUPS'] = cups
-
-            interpolate_df['measurementStart'] = interpolate_df['measurementStart'].astype('int') / 10 ** 9
-            interpolate_df['measurementStart'] = interpolate_df['measurementStart'].astype('int')
-
-            interpolate_df['measurementEnd'] = interpolate_df['measurementEnd'].astype('int') / 10 ** 9
-            interpolate_df['measurementEnd'] = interpolate_df['measurementEnd'].astype('int')
-
-            print(interpolate_df)
+    # todo: MeasureList Day
+    # todo: time series -> HBASE
+    # todo: inputs, workflows
