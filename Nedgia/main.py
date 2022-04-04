cc49e6cd1a6bbd4252ae59dbb41f3ad693f057c1
elskater98 <elskater98@gmail.com>
2022-04-04 13:06:33 +0200

    - Adapt code

--- Nedgia/main.py
+++ Nedgia/main.py
@@ -1,39 +1,36 @@
 import argparse
 import hashlib
+import re
 from datetime import datetime
 
 import pandas as pd
 from neo4j import GraphDatabase
 from rdflib import Namespace
 
-from utils.hbase import save_to_hbase
+from utils.hbase import save_to_hbase, get_hbase_data_batch
 from utils.utils import read_config
 
 tz_info = 'Europe/Madrid'
 
 
-def script_input():
-    df = pd.read_excel('../data/Generalitat Extracción_2020.xlsx', skiprows=2)
+def script_input(config):
+    hbase_table = "harmonized_ts_invoices_invoices_icaen"
+    hbase_conn = config['hbase_store_harmonized_data']
 
-    # Transform Raw Data
+    dic_list = []
+    for data in get_hbase_data_batch(hbase_conn, hbase_table):
+        print("parsing hbase")
+        for id_, x in data:
+            item = dict()
+            for k, v in x.items():
+                k1 = re.sub("^info:", "", k.decode("utf-8"))
+                item[k1] = v.decode("utf-8")
+            cups, ts = id_.decode("utf-8").split("~")
+            item.update({"CUPS": cups, "start": ts, "ts": ts})
+            dic_list.append(item)
 
-    df['Fecha fin Docu. cálculo'] += pd.Timedelta(hours=23)
-
-    df['Fecha inicio Docu. cálculo'] = df['Fecha inicio Docu. cálculo'].dt.tz_localize(tz_info)
-    df['Fecha fin Docu. cálculo'] = df['Fecha fin Docu. cálculo'].dt.tz_localize(tz_info)
-
-    # datatime64 [ns] to unix time
-    df['measurementStart'] = df['Fecha inicio Docu. cálculo'].astype('int') / 10 ** 9
-    df['measurementStart'] = df['measurementStart'].astype('int')
-
-    df['measurementEnd'] = df['Fecha fin Docu. cálculo'].astype('int') / 10 ** 9
-    df['measurementEnd'] = df['measurementEnd'].astype('int')
-    df['ts'] = df['measurementStart']
-
-    # Calculate kWh
-    df['measurementValue'] = df['Consumo kWh ATR'].fillna(0) + df['Consumo kWh GLP'].fillna(0)
-
-    df = df[['CUPS', 'ts', 'measurementStart', 'measurementEnd', 'measurementValue', 'Tipo Lectura']]
+    df = pd.DataFrame(dic_list)
+    df.rename(columns={"v:value": "value"}, inplace=True)
 
     return df
 
@@ -64,7 +61,6 @@ def split_dataframe(dataframe):
 
         for i in range(len(list_of_dataframes)):
             new_df = pd.DataFrame(list_of_dataframes[i])
-            new_df.rename(columns={'_6': 'Tipo Lectura'}, inplace=True)
             list_of_dataframes[i] = new_df
 
     else:
@@ -74,18 +70,16 @@ def split_dataframe(dataframe):
 
 
 def harmonize_dataframe(dataframe):
-    if 'REAL' in dataframe['Tipo Lectura'].unique():
+    if True in dataframe['isReal'].unique():
         # Drop n rows until REAL
-        first_real = dataframe[dataframe['Tipo Lectura'] == 'REAL'].index[0]
-
+        first_real = dataframe[dataframe['isReal'] == True].index[0]
         df_i = dataframe.loc[first_real:]
     else:
         return
 
-    # Filter by Tipo Lectura == REAL
-    df_i_real = df_i[df_i["Tipo Lectura"] == 'REAL'].copy()
+    df_i_real = df_i[df_i["isReal"] == True].copy()
 
-    df_i_real['cumsum'] = df_i_real['measurementValue'].cumsum()
+    df_i_real['cumsum'] = df_i_real['value'].cumsum()
 
     df_i_real.set_index('measurementStart_dt', inplace=True)
 
@@ -99,17 +93,17 @@ def harmonize_dataframe(dataframe):
 
     interpolate_df.reset_index(inplace=True)
 
-    interpolate_df.rename(columns={'measurementStart_dt': 'measurementStart', 'diff': 'measurementValue',
-                                   'measurementEnd_dt': 'measurementEnd'}, inplace=True)
+    interpolate_df.rename(columns={'measurementStart_dt': 'start', 'diff': 'value',
+                                   'measurementEnd_dt': 'end'}, inplace=True)
     interpolate_df['CUPS'] = cups
 
-    interpolate_df['measurementStart'] = interpolate_df['measurementStart'].astype('int') / 10 ** 9
-    interpolate_df['measurementStart'] = interpolate_df['measurementStart'].astype('int')
+    interpolate_df['start'] = interpolate_df['start'].astype('int') / 10 ** 9
+    interpolate_df['start'] = interpolate_df['start'].astype('int')
 
-    interpolate_df['measurementEnd'] = interpolate_df['measurementEnd'].astype('int') / 10 ** 9
-    interpolate_df['measurementEnd'] = interpolate_df['measurementEnd'].astype('int')
+    interpolate_df['end'] = interpolate_df['end'].astype('int') / 10 ** 9
+    interpolate_df['end'] = interpolate_df['end'].astype('int')
 
-    return interpolate_df[['CUPS', 'measurementStart', 'measurementEnd', 'measurementValue']]
+    return interpolate_df[['CUPS', 'start', 'end', 'value']]
 
 
 def create_measurement_list(df, device_id, args, config):
@@ -117,39 +111,45 @@ def create_measurement_list(df, device_id, args, config):
     neo = GraphDatabase.driver(**neo4j_connection)
 
     n = Namespace(args.namespace)
-    uri = n[f"{device_id}-DEVICE-{args.source}"]
 
     with neo.session() as session:
-        list_uri = n[f"{device_id}-DEVICE-{args.source}-LIST-PROJECTED-P1D"]
-        new_d_id = hashlib.sha256(list_uri.encode("utf-8"))
-        new_d_id = new_d_id.hexdigest()
-
-        dt_ini = df['measurementStart'].iloc[0]
-        dt_end = df['measurementEnd'].iloc[-1]
-
-        query_measures = f"""
-            MATCH (device: ns0__Device {{uri:"{uri}"}})
-            MERGE (list: ns0__MeasurementList{{uri: "{list_uri}", ns0__measurementKey: "{new_d_id}",
-            ns0__measurementFrequency: "P1D"}} )<-[:ns0__hasMeasurementLists]-(device)
-            SET
-                list.ns0__measurementUnit= "kWh",
-                list.ns0__measuredProperty= "gasConsumption",
-                list.ns0__measurementListStart = CASE
-                    WHEN list.ns0__measurementListStart <
-                     datetime("{datetime.fromtimestamp(dt_ini).isoformat()}")
-                        THEN list.ns0__measurementListStart
-                        ELSE datetime("{datetime.fromtimestamp(dt_ini).isoformat()}")
-                    END,
-                list.ns0__measurementListEnd = CASE
-                    WHEN list.ns0__measurementListEnd >
-                     datetime("{datetime.fromtimestamp(dt_end).isoformat()}")
-                        THEN list.ns0__measurementListEnd
-                        ELSE datetime("{datetime.fromtimestamp(dt_end).isoformat()}")
-                    END
-            return list
+        query = f"""
+        MATCH (n:ns0__MeasurementList{{ns0__measurementKey:"{device_id}"}})-[:ns0__hasMeasurementLists]-(b) return b
         """
+        for i in list(session.run(query)):
+            cups = i['b']['ns0__deviceName']
+            uri = n[f"{cups}-DEVICE-{args.source}"]
 
-    session.run(query_measures)
+            list_uri = n[f"{cups}-DEVICE-{args.source}-LIST-PROJECTED-P1D"]
+            new_d_id = hashlib.sha256(list_uri.encode("utf-8"))
+            new_d_id = new_d_id.hexdigest()
+
+            dt_ini = df['start'].iloc[0]
+            dt_end = df['end'].iloc[-1]
+
+            query_measures = f"""
+                MATCH (device: ns0__Device {{uri:"{uri}"}})
+                MERGE (list: ns0__MeasurementList{{uri: "{list_uri}", ns0__measurementKey: "{new_d_id}",
+                ns0__measurementFrequency: "P1D"}} )<-[:ns0__hasMeasurementLists]-(device)
+                SET
+                    list.ns0__measurementUnit= "kWh",
+                    list.ns0__measuredProperty= "gasConsumption",
+                    list.ns0__measurementListStart = CASE
+                        WHEN list.ns0__measurementListStart <
+                         datetime("{datetime.fromtimestamp(dt_ini).isoformat()}")
+                            THEN list.ns0__measurementListStart
+                            ELSE datetime("{datetime.fromtimestamp(dt_ini).isoformat()}")
+                        END,
+                    list.ns0__measurementListEnd = CASE
+                        WHEN list.ns0__measurementListEnd >
+                         datetime("{datetime.fromtimestamp(dt_end).isoformat()}")
+                            THEN list.ns0__measurementListEnd
+                            ELSE datetime("{datetime.fromtimestamp(dt_end).isoformat()}")
+                        END
+                return list
+            """
+
+        session.run(query_measures)
 
 
 def save_ts_to_hbase(df, device_id, args, config):
@@ -163,19 +163,20 @@ def save_ts_to_hbase(df, device_id, args, config):
 
     df['listKey'] = new_d_id
 
-    save_to_hbase(df.to_dict(orient="records"), f"bigg_ts_EnergyConsumptionGas_P1D_{args.user}", hbase_conn2,
-                  [("info", ['measurementEnd']), ("v", ['measurementValue'])],
-                  row_fields=['listKey', 'measurementStart'])
+    save_to_hbase(df.to_dict(orient="records"), f"harmonized_ts_EnergyConsumptionGas_P1D_{args.user}", hbase_conn2,
+                  [("info", ['end']), ("v", ['value'])],
+                  row_fields=['listKey', 'start'])
 
-    save_to_hbase(df.to_dict(orient="records"), f"bigg_analyticsTS_EnergyConsumptionGas_P1D_{args.user}", hbase_conn2,
-                  [("info", ['measurementEnd']), ("v", ['measurementValue'])],
-                  row_fields=['measurementStart', 'listKey'])
+    save_to_hbase(df.to_dict(orient="records"), f"harmonized_analyticsTs_EnergyConsumptionGas_P1D_{args.user}",
+                  hbase_conn2,
+                  [("info", ['end']), ("v", ['value'])],
+                  row_fields=['start', 'listKey'])
 
 
 if __name__ == '__main__':
     # Arguments
     ap = argparse.ArgumentParser(description='Mapping of Gas data to neo4j.')
-    ap.add_argument("--user", "-u", help="The source importing the data", required=True)
+    ap.add_argument("--user", "-u", help="The user importing the data", required=True)
     ap.add_argument("--source", "-so", help="The source importing the data", required=True)
     ap.add_argument("--namespace", "-n", help="The subjects namespace uri", required=True)
     ap.add_argument("--timezone", "-tz", help="The local timezone", required=True, default='Europe/Madrid')
@@ -185,19 +186,22 @@ if __name__ == '__main__':
     config = read_config('../config.json')
 
     # Load Input
-    df = script_input()
+    df = script_input(config)
 
     # Group by CUPS
     for cups, sub_df in df.groupby('CUPS'):
         # Convert Timestamps to Datetime
-        sub_df.sort_values(by=['measurementStart'], inplace=True)
-        sub_df['measurementStart_dt'] = pd.to_datetime(sub_df['measurementStart'], unit='s').dt.tz_localize(
+        sub_df.sort_values(by=['start'], inplace=True)
+        sub_df['measurementStart_dt'] = pd.to_datetime(sub_df['start'], unit='s').dt.tz_localize(
             'UTC').dt.tz_convert(tz_info)
 
-        sub_df['measurementEnd_dt'] = pd.to_datetime(sub_df['measurementEnd'], unit='s').dt.tz_localize(
+        sub_df['measurementEnd_dt'] = pd.to_datetime(sub_df['end'], unit='s').dt.tz_localize(
             'UTC').dt.tz_convert(tz_info)
 
         sub_df['measurementEnd_dt'] -= pd.Timedelta(hours=23)
+
+        sub_df['isReal'] = sub_df['isReal'].astype(bool)
+        sub_df['value'] = sub_df['value'].astype(int)
 
         # https://towardsdatascience.com/efficiently-iterating-over-rows-in-a-pandas-dataframe-7dd5f9992c01
 
